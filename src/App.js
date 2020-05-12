9e691b86c7e8509c3bdf38b2395ae2dbf02b82d4
Rakshit Shetty <rakshitshetty561998@gmail.com>
2020-05-12 17:07:58 +0530

    added firebase authentication

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Header from './components/header/header.component'
 
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor(){
@@ -25,10 +25,26 @@ class App extends React.Component {
 
   componentDidMount(){
     this.unsubscribeFromAuth = 
-    auth.onAuthStateChanged(user => {
-      this.setState({currentUser : user});
+    auth.onAuthStateChanged(async userAuth => {
+      //this.setState({currentUser : user});
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth); 
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState(
+            {
+              currentUser:{
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            },() => console.log(this.state)
+          )
+        })
+      }
+      else{
+        this.setState({currentUser : userAuth}); //userAuth is null
+
+      }
     })
   }
 
