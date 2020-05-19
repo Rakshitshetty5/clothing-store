7aa6c989e8bf4e496ab633a66e5a9506402ddcd9
Rakshit Shetty <rakshitshetty561998@gmail.com>
2020-05-19 18:17:55 +0530

    added increase and decrease quantinty for checkout page items

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,6 @@
 import { CartActionTypes } from './cart.types';
-import {addItemToCart} from './cart.utils'
+import {addItemToCart, removeItemFromCart} from './cart.utils'
+
 
 const INITIAL_STATE = {
     hidden : true,
@@ -26,6 +27,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                         cartItem.id !== action.payload.id
                     )
 
+            }
+        case CartActionTypes.REMOVE_ITEM:
+            return{
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
             }
         default : 
             return state;
